4f325b72c96214983fe4a087f4dc36f69cc71dd2
Guido Matos Camones <guido.matos.88@gmail.com>
2020-07-31 20:16:26 -0500

    Verde: Creando el proyecto de la implementación

---
+++ Calculadora/CalculadoraDeImpuestos.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Calculadora
+{
+    public class CalculadoraDeImpuestos
+    {
+        public decimal ObtenerIVA(decimal montoReserva) {
+            const decimal porcentaje = 0.16m;
+            return 0;
+        }
+    }
+}
